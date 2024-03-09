36a83d5aa77c6acea04ce4506f49103978468f78
eeuunn <haspring@dsm.hs.kr>
2024-03-10 00:26:58 +0900

    🍯 :: controller

---
+++ src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
@@ -0,0 +1,24 @@
+package com.example.removalbackend.domain.video.presentation
+
+import com.example.removalbackend.domain.video.presentation.dto.VideoRequest
+import com.example.removalbackend.domain.video.service.VideoService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/video")
+@RestController
+class VideoController(
+    private val videoService: VideoService
+) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    fun createVideo(@RequestBody @Valid request: VideoRequest){
+        videoService.createVideo(request)
+    }
+}
