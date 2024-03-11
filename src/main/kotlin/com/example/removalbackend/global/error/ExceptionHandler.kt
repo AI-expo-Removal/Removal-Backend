312edcacf9e9037ffe0ba45ef2e08e589a32a89d
eeuunn <haspring@dsm.hs.kr>
2024-03-11 22:17:13 +0900

    🍯 :: ExceptionHandler

---
+++ src/main/kotlin/com/example/removalbackend/global/error/ExceptionHandler.kt
@@ -0,0 +1,25 @@
+package com.example.removalbackend.global.error
+
+import com.example.removalbackend.global.error.exception.RemovalException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandler {
+    @ExceptionHandler(RemovalException::class)
+    fun customExceptionHandling(e: RemovalException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse.of(e), HttpStatus.valueOf(e.status))
+    }
+
+    @ExceptionHandler(BindException::class)
+    fun handleBindException(e: BindException): ResponseEntity<Map<String, String?>> {
+        val errorMap: MutableMap<String, String?> = HashMap()
+        for (error in e.fieldErrors) {
+            errorMap[error.field] = error.defaultMessage
+        }
+        return ResponseEntity<Map<String, String?>>(errorMap, HttpStatus.BAD_REQUEST)
+    }
+}
