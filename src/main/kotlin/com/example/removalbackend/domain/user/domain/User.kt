476397f30d6f39e867e95d4e694a72450af1c643
eeuunn <haspring@dsm.hs.kr>
2024-03-12 23:11:47 +0900

    🍯 :: User

--- src/main/kotlin/com/example/removalbackend/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/domain/User.kt
@@ -1,28 +1,25 @@
 package com.example.removalbackend.domain.user.domain
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
+import javax.persistence.*
 
 @Entity(name = "tbl_user")
 class User(
     @Id
-    @Column(length = 15, nullable = false)
+    @Column(columnDefinition = "CHAR(20)", nullable = false)
     var accountId : String,
 
-    @Column(length = 20, nullable = false)
+    @Column(columnDefinition = "CHAR(60)", nullable = false)
     var password: String,
 
-    @Column(length = 5, nullable = false)
+    @Column(columnDefinition = "CHAR(5)", nullable = false)
     var name: String,
 
-    @Column(length = 12, nullable = false)
+    @Column(columnDefinition = "CHAR(12)", nullable = false)
     var phoneNumber: String
 ){
     @JsonCreator
-    protected constructor() : this("", "", "", "")
+    protected constructor() : this( accountId = "user123", password = "password123", name = "hahahaha", phoneNumber = "123456789")
 
     fun updatePassword(password: String){
         this.password = password
