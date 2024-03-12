f950b7e603507feec419fc930c523eab6993d944
eeuunn <haspring@dsm.hs.kr>
2024-03-12 23:15:48 +0900

    🍯 :: BaseUUIDEntity

--- src/main/kotlin/com/example/removalbackend/global/entity/BaseUUIDEntity.kt
+++ src/main/kotlin/com/example/removalbackend/global/entity/BaseUUIDEntity.kt
@@ -1,9 +1,9 @@
 package com.example.removalbackend.global.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.MappedSuperclass
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
 import org.hibernate.annotations.GenericGenerator
 import java.util.*
 
