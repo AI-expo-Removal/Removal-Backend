826ce2f626d176fac1e7420dcc11d5b856d138e0
eeuunn <haspring@dsm.hs.kr>
2024-03-12 23:13:35 +0900

    🍯 :: SignUpRequest

--- src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/request/SignUpRequest.kt
@@ -1,8 +1,8 @@
 package com.example.removalbackend.domain.user.presentation.dto.request
 
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Pattern
-import jakarta.validation.constraints.Size
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 data class SignUpRequest(
     @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
@@ -10,7 +10,11 @@ data class SignUpRequest(
     val accountId: String,
 
     @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @field: Size(min = 8, max = 20, message = "8자 ~ 20자 까지 허용합니다")
+    @field:Pattern(
+        regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
+                "<=>?@＼^_`{|}~]{8,30}$",
+        message = "소문자, 숫자, 특수문자가 포함되어야 하며 8자~20자 사이여야 합니다."
+    )
     val password: String,
 
     @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
@@ -18,10 +22,5 @@ data class SignUpRequest(
     val name: String,
 
     @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @field:Pattern(
-        regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
-                "<=>?@＼^_`{|}~]{8,30}$",
-        message = "소문자, 숫자, 특수문자가 포함되어야 하며 8자~20자 사이여야 합니다."
-    )
     val phoneNumber: String,
 )
