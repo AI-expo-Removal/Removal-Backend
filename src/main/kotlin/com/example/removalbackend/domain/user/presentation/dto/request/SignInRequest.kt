88b2b541b0feacd5cef13490eb3d3aa89e3f0102
eeuunn <haspring@dsm.hs.kr>
2024-03-12 23:12:23 +0900

    🍯 :: SignInRequest

--- src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/request/SignInRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/request/SignInRequest.kt
@@ -1,6 +1,6 @@
 package com.example.removalbackend.domain.user.presentation.dto.request
 
-import jakarta.validation.constraints.NotBlank
+import javax.validation.constraints.NotBlank
 
 class SignInRequest(
     @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
