08c3ac77e01747117d24a8a60bc345882e6053c3
eeuunn <haspring@dsm.hs.kr>
2024-03-12 23:11:41 +0900

    🍯 :: UserController

---
+++ src/main/kotlin/com/example/removalbackend/domain/user/presentation/UserController.kt
@@ -0,0 +1,52 @@
+package com.example.removalbackend.domain.user.presentation
+
+import com.example.removalbackend.domain.user.presentation.dto.request.SignInRequest
+import com.example.removalbackend.domain.user.presentation.dto.request.SignUpRequest
+import com.example.removalbackend.domain.user.presentation.dto.response.TokenResponse
+import com.example.removalbackend.domain.user.service.SignInService
+import com.example.removalbackend.domain.user.service.SignUpService
+import com.example.removalbackend.global.security.jwt.TokenProvider
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RequestMapping("/users")
+@RestController
+class UserController(
+    private val signInService: SignInService,
+    private val signUpService: SignUpService,
+    private val tokenProvider: TokenProvider
+
+    ) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    fun signUp(@RequestBody @Valid request: SignUpRequest) {
+        signUpService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/login")
+    fun signIn(@RequestBody @Valid request: SignInRequest) :TokenResponse {
+        return signInService.execute(request)
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/token")
+    fun refreshToken(@RequestHeader("Authorization") authorization: String): ResponseEntity<Any> {
+        val token = authorization.removePrefix("Bearer").trim()
+
+        return if (tokenProvider.validateToken(token)) {
+            val newToken = tokenProvider.generateToken(token)
+            ResponseEntity.ok().body(mapOf("token" to newToken))
+        } else {
+            ResponseEntity.badRequest().body("Invalid token")
+        }
+    }
+}
