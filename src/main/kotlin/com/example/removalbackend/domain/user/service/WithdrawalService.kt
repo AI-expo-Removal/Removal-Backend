3c64c5312d3e9c2e20973c7ada6f24699cc4e537
eeuunn <haspring@dsm.hs.kr>
2024-03-13 20:00:35 +0900

    🍯 :: Withdrawal

---
+++ src/main/kotlin/com/example/removalbackend/domain/user/service/WithdrawalService.kt
@@ -0,0 +1,16 @@
+package com.example.removalbackend.domain.user.service
+
+import com.example.removalbackend.domain.user.domain.repository.UserRepository
+import com.example.removalbackend.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+
+@Service
+class WithdrawalService(
+    private val userRepository: UserRepository,
+    private val userFacade: UserFacade
+) {
+    fun memberDelete(){
+        val user = userFacade.getCurrentUser()
+        return userRepository.delete(user)
+    }
+}
