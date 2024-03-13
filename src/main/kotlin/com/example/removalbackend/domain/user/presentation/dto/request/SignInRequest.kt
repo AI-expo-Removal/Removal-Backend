329670240f0d1ad3f36d768d758f2f834a20a1a2
eeuunn <haspring@dsm.hs.kr>
2024-03-13 10:37:02 +0900

    🍯 :: SignInRequest

--- src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/request/SignInRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/request/SignInRequest.kt
@@ -2,7 +2,7 @@ package com.example.removalbackend.domain.user.presentation.dto.request
 
 import javax.validation.constraints.NotBlank
 
-class SignInRequest(
+data class SignInRequest(
     @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
     val accountId: String,
 
