11e591b3c7034298ecc6979c78bb5833d5f737ad
eeuunn <haspring@dsm.hs.kr>
2024-03-18 09:32:56 +0900

    🍯 :: Authentication number match being edited

--- src/main/kotlin/com/example/removalbackend/domain/sms/repository/SmsCertification.kt
+++ src/main/kotlin/com/example/removalbackend/domain/sms/repository/SmsCertification.kt
@@ -7,7 +7,7 @@ import java.time.Duration
 @Repository
 class SmsCertification(private val stringRedisTemplate: StringRedisTemplate) {
     private val PREFIX = "sms:" // key값이 중복되지 않도록 상수 선언
-    private val LIMIT_TIME = 3 * 60 // 인증번호 유효 시간
+    private val LIMIT_TIME = 5 * 60 // 인증번호 유효 시간
 
     // Redis에 저장
     fun createSmsCertification(phone: String, certificationNumber: String) {
@@ -16,17 +16,17 @@ class SmsCertification(private val stringRedisTemplate: StringRedisTemplate) {
     }
 
     // 휴대전화 번호에 해당하는 인증번호 불러오기
-    fun getSmsCertification(phone: String): String? {
-        return stringRedisTemplate.opsForValue().get("$PREFIX$phone")
+    fun getSmsCertification(phoneNumber: String): String? {
+        return stringRedisTemplate.opsForValue().get("$PREFIX$phoneNumber")
     }
 
     // 인증 완료 시, 인증번호 Redis에서 삭제
-    fun deleteSmsCertification(phone: String) {
-        stringRedisTemplate.delete("$PREFIX$phone")
+    fun deleteSmsCertification(phoneNumber: String) {
+        stringRedisTemplate.delete("$PREFIX$phoneNumber")
     }
 
     // Redis에 해당 휴대번호로 저장된 인증번호가 존재하는지 확인
-    fun hasKey(phone: String): Boolean {
-        return stringRedisTemplate.hasKey("$PREFIX$phone")
+    fun hasKey(phoneNumber: String): Boolean {
+        return stringRedisTemplate.hasKey("$PREFIX$phoneNumber")
     }
 }
