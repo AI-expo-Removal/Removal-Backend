5a230afa883403a42f322cdd93e9f90e7352d23a
eeuunn <haspring@dsm.hs.kr>
2024-03-18 09:32:25 +0900

    🍯 :: Request to confirm authentication number

--- src/main/kotlin/com/example/removalbackend/domain/sms/presentation/SmsController.kt
+++ src/main/kotlin/com/example/removalbackend/domain/sms/presentation/SmsController.kt
@@ -1,7 +1,10 @@
 package com.example.removalbackend.domain.sms.presentation
 
 import com.example.removalbackend.domain.sms.presentation.dto.request.SmsRequest
+import com.example.removalbackend.domain.sms.presentation.dto.request.VerifySmsRequest
+import com.example.removalbackend.domain.sms.presentation.dto.response.SmsResponse
 import com.example.removalbackend.domain.sms.service.SmsService
+import com.example.removalbackend.domain.user.presentation.dto.request.SignUpRequest
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.*
 class SmsController(
     private val smsService: SmsService
 ) {
-
     @PostMapping
-    fun sendSms(@RequestBody smsRequest: SmsRequest): String {
-        smsService.sendSMS(smsRequest)
-        return "SMS 발송 완료"
+    fun sendSms(@RequestBody smsRequest: SmsRequest): SmsResponse{
+        return smsService.sendSMS(smsRequest)
+    }
+
+    @GetMapping
+    fun verifySms(@RequestBody verifySmsRequest: VerifySmsRequest): String{
+        return smsService.verifySms(verifySmsRequest)
     }
 }
