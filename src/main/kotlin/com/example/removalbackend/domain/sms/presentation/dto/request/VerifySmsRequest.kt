8105f51215513d546eb017ddfa75c0a5219b7921
eeuunn <haspring@dsm.hs.kr>
2024-03-21 15:11:20 +0900

    🍯 :: Sort code and remove unnecessary fields

--- src/main/kotlin/com/example/removalbackend/domain/sms/presentation/dto/request/VerifySmsRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/sms/presentation/dto/request/VerifySmsRequest.kt
@@ -4,8 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import javax.validation.constraints.NotBlank
 
 data class VerifySmsRequest(
-    @field: NotBlank(message = "null, 공백, 띄어쓰기를 허용하지 않습니다.")
     val phoneNumber: String,
 
-    @JsonProperty("randomNumber") val randomNumber: String
+    @JsonProperty("randomNumber")
+    val randomNumber: String
 )
