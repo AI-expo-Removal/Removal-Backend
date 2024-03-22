f78bc8f849e708d36c3a87351ebe3986fae34d64
eeuunn <haspring@dsm.hs.kr>
2024-03-23 00:03:08 +0900

    🍯 :: randomNumber return delete

--- src/main/kotlin/com/example/removalbackend/domain/sms/presentation/SmsController.kt
+++ src/main/kotlin/com/example/removalbackend/domain/sms/presentation/SmsController.kt
@@ -2,7 +2,6 @@ package com.example.removalbackend.domain.sms.presentation
 
 import com.example.removalbackend.domain.sms.presentation.dto.request.SmsRequest
 import com.example.removalbackend.domain.sms.presentation.dto.request.VerifySmsRequest
-import com.example.removalbackend.domain.sms.presentation.dto.response.SmsResponse
 import com.example.removalbackend.domain.sms.service.SmsService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -15,17 +14,13 @@ class SmsController(
 ) {
     @ResponseStatus(HttpStatus.OK)
     @PostMapping()
-    fun sendSms(@RequestBody smsRequest: SmsRequest): SmsResponse {
+    fun sendSms(@RequestBody smsRequest: SmsRequest) {
         return smsService.sendSMS(smsRequest)
     }
 
     @GetMapping()
-    fun verifySms(@RequestBody request: VerifySmsRequest): ResponseEntity<Any> {
-        return try {
-            val message = smsService.verifySms(request)
-            ResponseEntity.ok().body(mapOf("message" to message))
-        } catch (e: IllegalArgumentException) {
-            ResponseEntity.badRequest().body(mapOf("error" to e.message))
-        }
+    fun verifySms(@RequestBody request: VerifySmsRequest): String {
+        return smsService.verifySms(request)
+
     }
 }
