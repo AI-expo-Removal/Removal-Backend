3757776e2bb4096c8d695ce7b7caf5b7070f5f9b
eeuunn <haspring@dsm.hs.kr>
2024-03-27 11:49:46 +0900

    🍯 :: map phoneNumber to json

--- src/main/kotlin/com/example/removalbackend/domain/sms/presentation/dto/request/SmsRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/sms/presentation/dto/request/SmsRequest.kt
@@ -3,5 +3,6 @@ package com.example.removalbackend.domain.sms.presentation.dto.request
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class SmsRequest(
-    @JsonProperty("phoneNumber") val phoneNumber: String
+    @JsonProperty("phoneNumber")
+    val phoneNumber: String
 )
