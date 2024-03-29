6efdbbfe6efd1199fad7f8541c1741f0f5702db8
eeuunn <haspring@dsm.hs.kr>
2024-03-29 21:25:30 +0900

    🍯 :: VideoRepository

--- src/main/kotlin/com/example/removalbackend/domain/video/domain/repository/VideoRepository.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/domain/repository/VideoRepository.kt
@@ -1,6 +1,6 @@
 package com.example.removalbackend.domain.video.domain.repository
 
-import com.example.removalbackend.domain.video.domain.Video
+import com.example.removalbackend.domain.title.domain.Video
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface VideoRepository : JpaRepository<Video, Long> {
