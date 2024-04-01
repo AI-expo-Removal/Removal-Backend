b83a8af76f17135b30c0b454ff241e47e0c713fb
eeuunn <haspring@dsm.hs.kr>
2024-04-02 08:38:56 +0900

    🍯 :: Code still being modified

--- src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
@@ -1,6 +1,7 @@
 package com.example.removalbackend.domain.video.presentation
 
 import com.example.removalbackend.domain.video.presentation.dto.request.VideoTitleRequest
+import com.example.removalbackend.domain.video.presentation.dto.response.VideoResponse
 import com.example.removalbackend.domain.video.service.VideoService
 import lombok.RequiredArgsConstructor
 import lombok.Value
@@ -8,13 +9,16 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.io.IOException
+import javax.annotation.Resource
 import javax.validation.Valid
 
 @RequestMapping("/video")
@@ -23,26 +27,19 @@ import javax.validation.Valid
 class VideoController(
     private val videoService: VideoService,
 ) {
-    @GetMapping()
-    fun chunkUploadPage(): String {
-        return "chunk"
-    }
-
-    @ResponseBody
-    @PostMapping()
-    @Throws(IOException::class)
-    fun chunkUpload(
-        @RequestParam("chunk") file: MultipartFile?,
-        @RequestParam("chunkNumber") chunkNumber: Int,
-        @RequestParam("totalChunks") totalChunks: Int
-    ): ResponseEntity<String> {
-        val isDone = videoService!!.uploadFile(file!!, chunkNumber, totalChunks)
-        return if (isDone) ResponseEntity.ok<String>("File uploaded successfully") else ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
-            .build<String>()
-    }
+//    @PostMapping()
+//    fun uploadVideoFile(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
+//        val fileName = videoService.uploadFile(file)
+//        val fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+//            .path("/videos/")
+//            .path(fileName)
+//            .toUriString()
+//        return ResponseEntity.ok().body(fileDownloadUri)
+//    }
 
     @PostMapping("/title")
-    fun saveTitle(@RequestBody @Valid request : VideoTitleRequest){
-        videoService.saveTitle(request)
+    fun saveTitle(@RequestBody @Valid request : VideoTitleRequest, @RequestParam("file") file: MultipartFile): VideoResponse{
+        videoService.saveTitle(request,file)
+        return VideoResponse(request.videoUrl)
     }
 }
