5b30b8fd5b35f7ea9d66093163105049b92bb9b1
eeuunn <haspring@dsm.hs.kr>
2024-04-02 08:38:07 +0900

    🍯 :: ADD Jsonproperty and ADD videoUrl

--- src/main/kotlin/com/example/removalbackend/domain/video/presentation/dto/request/VideoTitleRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/presentation/dto/request/VideoTitleRequest.kt
@@ -1,5 +1,10 @@
 package com.example.removalbackend.domain.video.presentation.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class VideoTitleRequest(
+    val videoUrl: String,
+
+    @JsonProperty("title")
     val title: String
 )
