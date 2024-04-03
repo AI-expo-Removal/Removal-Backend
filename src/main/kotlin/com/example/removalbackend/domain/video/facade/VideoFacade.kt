b45fcf61a982e3952fdf40361e19e23fd3f7d627
eeuunn <haspring@dsm.hs.kr>
2024-04-03 11:14:47 +0900

    🍯 :: ADD VideoFacade

---
+++ src/main/kotlin/com/example/removalbackend/domain/video/facade/VideoFacade.kt
@@ -0,0 +1,15 @@
+package com.example.removalbackend.domain.video.facade
+
+import com.example.removalbackend.domain.title.domain.Video
+import com.example.removalbackend.domain.video.domain.repository.VideoRepository
+import com.example.removalbackend.domain.video.exception.VideoNotFoundException
+import org.springframework.stereotype.Component
+
+@Component
+class VideoFacade(
+    private val videoRepository: VideoRepository
+) {
+    fun getById(videoId: Long): Video{
+        return videoRepository.findById(videoId).orElseThrow { VideoNotFoundException }
+    }
+}
