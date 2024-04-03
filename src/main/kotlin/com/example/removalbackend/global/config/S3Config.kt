79311c487393208dd57ce486b6ce6e9d5433cd07
eeuunn <haspring@dsm.hs.kr>
2024-04-03 09:49:55 +0900

    🍯 :: UPDATE S3Config

--- src/main/kotlin/com/example/removalbackend/global/config/S3Config.kt
+++ src/main/kotlin/com/example/removalbackend/global/config/S3Config.kt
@@ -15,41 +15,14 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner
 
 @Configuration
 class S3Config {
+    @Value("\${cloud.aws.credentials.access-key}")
+    private lateinit var accessKey: String
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    private lateinit var secretKey: String
+
     @Value("\${cloud.aws.region.static}")
     private lateinit var region: String
-
-    @Value("\${AWS_S3_ACCESS}")
-    private val accessKey: String? = null
-
-    @Value("\${AWS_S3_SECRET}")
-    private val secretKey: String? = null
-
-    val videoFolder = "video"
-
-    companion object {
-        const val videoFolder = "video"
-    }
-
-    @Bean
-    fun s3Client(): S3Client {
-        return S3Client.builder()
-            .region(Region.of(region))
-            .build()
-    }
-
-    @Bean
-    fun s3Presigner(): S3Presigner {
-        return S3Presigner.builder()
-            .region(Region.of(region))
-            .build()
-    }
-
-    @Bean
-    fun amazonS3(): AmazonS3 {
-        return AmazonS3ClientBuilder.standard()
-            .withRegion(region)
-            .build()
-    }
 
     @Bean
     fun amazonS3Client(): AmazonS3Client {
