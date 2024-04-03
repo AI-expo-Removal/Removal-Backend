0d7f422cda79b93ff76351459c2333ab1d8b6045
eeuunn <haspring@dsm.hs.kr>
2024-04-03 09:49:28 +0900

    🍯 :: ADD VideoController

--- src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
@@ -3,6 +3,7 @@ package com.example.removalbackend.domain.video.presentation
 import com.example.removalbackend.domain.video.presentation.dto.request.VideoTitleRequest
 import com.example.removalbackend.domain.video.presentation.dto.response.VideoResponse
 import com.example.removalbackend.domain.video.service.VideoService
+import com.example.removalbackend.domain.video.service.VideoUploadService
 import lombok.RequiredArgsConstructor
 import lombok.Value
 import org.springframework.http.HttpStatus
@@ -14,32 +15,29 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import java.io.IOException
 import javax.annotation.Resource
 import javax.validation.Valid
 
-@RequestMapping("/video")
-@Controller
+@RequestMapping("/file")
+@RestController
 @RequiredArgsConstructor
 class VideoController(
     private val videoService: VideoService,
+    private val videoUploadService: VideoUploadService
 ) {
-//    @PostMapping()
-//    fun uploadVideoFile(@RequestParam("file") file: MultipartFile): ResponseEntity<String> {
-//        val fileName = videoService.uploadFile(file)
-//        val fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//            .path("/videos/")
-//            .path(fileName)
-//            .toUriString()
-//        return ResponseEntity.ok().body(fileDownloadUri)
-//    }
+    @PostMapping("/video")
+    fun saveFile(@RequestPart("file") multiPartFile: MultipartFile): VideoResponse {
+        return videoService.uploadVideo(multiPartFile)
+    }
 
-    @PostMapping("/title")
-    fun saveTitle(@RequestBody @Valid request : VideoTitleRequest, @RequestParam("file") file: MultipartFile): VideoResponse{
-        videoService.saveTitle(request,file)
-        return VideoResponse(request.videoUrl)
+    @PostMapping
+    fun saveTitle(@RequestBody request: VideoTitleRequest){
+        return videoUploadService.videoTitleService(request)
     }
 }
