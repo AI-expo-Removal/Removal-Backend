32a7920cb4c7024fdf66627bc1c0bd918cbb1b7b
eeuunn <haspring@dsm.hs.kr>
2024-04-03 09:49:14 +0900

    🍯 :: ADD VideoUploadService

---
+++ src/main/kotlin/com/example/removalbackend/domain/video/service/VideoUploadService.kt
@@ -0,0 +1,20 @@
+package com.example.removalbackend.domain.video.service
+
+import com.example.removalbackend.domain.title.domain.Video
+import com.example.removalbackend.domain.user.facade.UserFacade
+import com.example.removalbackend.domain.video.domain.repository.VideoRepository
+import com.example.removalbackend.domain.video.presentation.dto.request.VideoTitleRequest
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class VideoUploadService(
+    private val userFacade: UserFacade,
+    private val videoRepository : VideoRepository
+) {
+    fun videoTitleService(request: VideoTitleRequest){
+        val user = userFacade.getCurrentUser()
+        val video = Video(0, request.title, LocalDateTime.now(), request.videoUrl, user)
+        videoRepository.save(video)
+    }
+}
