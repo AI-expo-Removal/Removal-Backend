99bd05e9265f588d4f9a41a9aa1a5d3d165f1962
eeuunn <haspring@dsm.hs.kr>
2024-04-04 17:21:56 +0900

    🍯 :: ADD VideoRepository

--- src/main/kotlin/com/example/removalbackend/domain/video/domain/repository/VideoRepository.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/domain/repository/VideoRepository.kt
@@ -4,5 +4,4 @@ import com.example.removalbackend.domain.title.domain.Video
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface VideoRepository : JpaRepository<Video, Long> {
-    fun findByVideoId(videoId: Long): Video
 }
