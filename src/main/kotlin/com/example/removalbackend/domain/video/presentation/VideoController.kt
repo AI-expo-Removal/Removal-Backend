45bfc88da58b1023c96ebed2cccec5e9c4d946c9
eeuunn <haspring@dsm.hs.kr>
2024-04-04 15:31:55 +0900

    🍯 :: ADD updateVideoTitle

--- src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/presentation/VideoController.kt
@@ -1,38 +1,33 @@
 package com.example.removalbackend.domain.video.presentation
 
+import com.example.removalbackend.domain.video.exception.IncorrectUserException
 import com.example.removalbackend.domain.video.presentation.dto.request.VideoTitleRequest
 import com.example.removalbackend.domain.video.presentation.dto.request.VideoTitleUpdateRequest
 import com.example.removalbackend.domain.video.presentation.dto.response.VideoResponse
 import com.example.removalbackend.domain.video.service.VideoService
 import com.example.removalbackend.domain.video.service.VideoTitleUpdateService
-import com.example.removalbackend.domain.video.service.VideoUploadService
+import com.example.removalbackend.domain.video.service.VideoTitleUploadService
 import lombok.RequiredArgsConstructor
-import lombok.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
-import java.io.IOException
-import javax.annotation.Resource
-import javax.validation.Valid
+import java.io.File
 
 @RequestMapping("/file")
 @RestController
 @RequiredArgsConstructor
 class VideoController(
     private val videoService: VideoService,
-    private val videoUploadService: VideoUploadService,
+    private val videoUploadService: VideoTitleUploadService,
     private val videoTitleUpdateService: VideoTitleUpdateService
 ) {
     @PostMapping("/video")
@@ -41,12 +36,23 @@ class VideoController(
     }
 
     @PostMapping
-    fun saveTitle(@RequestBody request: VideoTitleRequest){
+    fun saveTitle(@RequestBody request: VideoTitleRequest) {
         return videoUploadService.videoTitleService(request)
     }
-
     @PatchMapping
-    fun patchTitle(@RequestBody request: VideoTitleUpdateRequest){
-        return videoTitleUpdateService.titleUpdate(request)
+    fun updateVideoTitle(
+        @RequestBody request: VideoTitleUpdateRequest,
+        @RequestHeader("userId") userId: String
+    ): ResponseEntity<String> {
+        try {
+            videoTitleUpdateService.titleUpdate(request)
+            return ResponseEntity.ok("동영상 제목이 성공적으로 수정되었습니다.")
+        } catch (e: IncorrectUserException) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("사용자는 이 비디오의 소유자가 아닙니다.")
+        } catch (e: Exception) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("동영상 제목을 수정하는 도중 오류가 발생했습니다.")
+        }
     }
 }
