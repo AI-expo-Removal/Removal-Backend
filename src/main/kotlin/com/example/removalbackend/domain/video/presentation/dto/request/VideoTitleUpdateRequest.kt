0751aad25371bcadda487a20cf5ea70d2fe58b80
eeuunn <haspring@dsm.hs.kr>
2024-04-04 15:31:25 +0900

    🍯 :: UPDATE VideoTitleUpdateRequest

--- src/main/kotlin/com/example/removalbackend/domain/video/presentation/dto/request/VideoTitleUpdateRequest.kt
+++ src/main/kotlin/com/example/removalbackend/domain/video/presentation/dto/request/VideoTitleUpdateRequest.kt
@@ -3,7 +3,8 @@ package com.example.removalbackend.domain.video.presentation.dto.request
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class VideoTitleUpdateRequest(
-    val videoUrl : String,
+    val videoId: Long,
+    val videoUrl: String,
 
     @JsonProperty("title")
     val title: String
