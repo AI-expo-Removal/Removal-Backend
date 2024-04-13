89fb28a7f9cd2bffc486d412d63f43387154d95b
eeuunn <haspring@dsm.hs.kr>
2024-04-13 12:25:38 +0900

    🍯 :: userInfoResponse phoneNumber

--- src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/response/UserInfoResponse.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/response/UserInfoResponse.kt
@@ -3,5 +3,5 @@ package com.example.removalbackend.domain.user.presentation.dto.response
 class UserInfoResponse(
     val accountId: String,
     val name: String,
-//    val phoneNumber: String
+    val phoneNumber: String
 )
