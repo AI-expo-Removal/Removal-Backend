52398976898ada3b23b6165331773c8f3db99035
eeuunn <haspring@dsm.hs.kr>
2024-05-02 01:44:09 +0900

    커밋이야 휴ㅜㅠㅜ 주석 제거

--- src/main/kotlin/com/example/removalbackend/domain/user/service/UpdatePasswordService.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/service/UpdatePasswordService.kt
@@ -1,19 +1,19 @@
-//package com.example.removalbackend.domain.user.service
-//
-//import com.example.removalbackend.domain.user.domain.repository.UserRepository
-//import com.example.removalbackend.domain.user.facade.UserFacade
-//import com.example.removalbackend.domain.user.presentation.dto.request.UpdatePasswordRequest
-//import org.springframework.stereotype.Service
-//
-//@Service
-//class UpdatePasswordService(
-//    private val userRepository: UserRepository,
-//    private val userFacade: UserFacade
-//) {
-//    fun updatePassword(request: UpdatePasswordRequest) {
-//        val user = userFacade.getCurrentUser()
-//        user.updatePassword(request.password)
-//
-//        userRepository.save(user)
-//    }
-//}
+package com.example.removalbackend.domain.user.service
+
+import com.example.removalbackend.domain.user.domain.repository.UserRepository
+import com.example.removalbackend.domain.user.facade.UserFacade
+import com.example.removalbackend.domain.user.presentation.dto.request.UpdatePasswordRequest
+import org.springframework.stereotype.Service
+
+@Service
+class UpdatePasswordService(
+    private val userRepository: UserRepository,
+    private val userFacade: UserFacade
+) {
+    fun updatePassword(request: UpdatePasswordRequest) {
+        val user = userFacade.getCurrentUser()
+        user.updatePassword(request.password)
+
+        userRepository.save(user)
+    }
+}
