30d3c2f8143dfb506317b04d81a6d972bc76ff77
eeuunn <haspring@dsm.hs.kr>
2024-05-03 00:23:15 +0900

    주석 제거

--- src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/response/UserInfoResponse.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/presentation/dto/response/UserInfoResponse.kt
@@ -1,7 +1,7 @@
-//package com.example.removalbackend.domain.user.presentation.dto.response
-//
-//class UserInfoResponse(
-//    val accountId: String,
-//    val name: String,
-////    val phoneNumber: String
-//)
+package com.example.removalbackend.domain.user.presentation.dto.response
+
+class UserInfoResponse(
+    val accountId: String,
+    val name: String,
+//    val phoneNumber: String
+)
