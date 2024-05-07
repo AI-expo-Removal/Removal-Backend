54d7e44c056cd11b2e2082eefb0577b958149627
eeuunn <haspring@dsm.hs.kr>
2024-05-07 14:21:30 +0900

    remomval bye

--- src/main/kotlin/com/example/removalbackend/domain/user/service/SignUpService.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/service/SignUpService.kt
@@ -1,35 +1,35 @@
-//package com.example.removalbackend.domain.user.service
-//
-//import com.example.removalbackend.domain.user.domain.User
-//import com.example.removalbackend.domain.user.domain.repository.UserRepository
-//import com.example.removalbackend.domain.user.presentation.dto.request.SignUpRequest
-//import javax.transaction.Transactional
-//import org.springframework.http.HttpStatus
-//import org.springframework.security.crypto.password.PasswordEncoder
-//import org.springframework.stereotype.Service
-//import org.springframework.web.server.ResponseStatusException
-//
-//@Service
-//class SignUpService(
-//    private val userRepository: UserRepository,
-//    private val passwordEncoder: PasswordEncoder
-//) {
-//    @Transactional
-//    fun execute(request: SignUpRequest) {
-//        if (!checkUser(request.accountId)) {
-//            val newUser = User(
-//                request.accountId,
-//                passwordEncoder.encode(request.password),
-//                request.name,
-////                request.phoneNumber
-//            )
-//            userRepository.save(newUser)
-//        } else {
-//            throw ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 ${request.accountId} 아이디 입니다.") as Throwable
-//        }
-//    }
-//
-//    fun checkUser(accountId: String): Boolean {
-//        return userRepository.existsByAccountId(accountId)
-//    }
-//}
+package com.example.removalbackend.domain.user.service
+
+import com.example.removalbackend.domain.user.domain.User
+import com.example.removalbackend.domain.user.domain.repository.UserRepository
+import com.example.removalbackend.domain.user.presentation.dto.request.SignUpRequest
+import javax.transaction.Transactional
+import org.springframework.http.HttpStatus
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class SignUpService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) {
+    @Transactional
+    fun execute(request: SignUpRequest) {
+        if (!checkUser(request.accountId)) {
+            val newUser = User(
+                request.accountId,
+                passwordEncoder.encode(request.password),
+                request.name,
+//                request.phoneNumber
+            )
+            userRepository.save(newUser)
+        } else {
+            throw ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 ${request.accountId} 아이디 입니다.") as Throwable
+        }
+    }
+
+    fun checkUser(accountId: String): Boolean {
+        return userRepository.existsByAccountId(accountId)
+    }
+}
--- src/main/kotlin/com/example/removalbackend/domain/user/service/UserInfoService.kt
+++ src/main/kotlin/com/example/removalbackend/domain/user/service/UserInfoService.kt
@@ -1,15 +1,15 @@
-//package com.example.removalbackend.domain.user.service
-//
-//import com.example.removalbackend.domain.user.facade.UserFacade
-//import com.example.removalbackend.domain.user.presentation.dto.response.UserInfoResponse
-//import org.springframework.stereotype.Service
-//
-//@Service
-//class UserInfoService(
-//    private val userFacade: UserFacade
-//) {
-//    fun execute(): UserInfoResponse {
-//        val user = userFacade.getCurrentUser()
-//        return UserInfoResponse(user.accountId, user.name)
-//    }
-//}
+package com.example.removalbackend.domain.user.service
+
+import com.example.removalbackend.domain.user.facade.UserFacade
+import com.example.removalbackend.domain.user.presentation.dto.response.UserInfoResponse
+import org.springframework.stereotype.Service
+
+@Service
+class UserInfoService(
+    private val userFacade: UserFacade
+) {
+    fun execute(): UserInfoResponse {
+        val user = userFacade.getCurrentUser()
+        return UserInfoResponse(user.accountId, user.name)
+    }
+}
